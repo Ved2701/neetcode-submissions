754f6be2d6de8bc6e0e33c975097ee0da5ac93b2
Ved <vedgarcia3@gmail.com>
2026-07-21 03:05:20 +0530

    Add: remove-node-from-end-of-linked-list - submission-0

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n)  {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (n > 0){
+            fast = fast->next;
+            n--;
+        }
+        if (!fast) return head->next; 
+        while (fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};
